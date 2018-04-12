17f2df8e3deded2c4a377664bf6d8a66d6f7e0d1
darkashli <A.Darkashli@aspect-ict.nl>
2018-04-12 12:38:36 +0200

    Search function demo

---
+++ SearchFunctionalityDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using SearchFunctionalityDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchFunctionalityDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+        public ActionResult Index(string searching)
+        {
+            if (string.IsNullOrEmpty(searching))
+            {
+                return View(db.Employees.ToList());
+            }
+            else
+            {
+                return View(db.Employees.Where(x => x.FirstName.StartsWith(searching)).ToList());
+            }
+        }
+
+    }
+}
---
+++ SearchFunctionalityDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SearchFunctionalityDemo.MvcApplication" Language="C#" %>
